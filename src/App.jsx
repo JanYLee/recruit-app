0ba57f338543cfc3f658f2b4b779578caa99fd5f
JanYLee <694730359@qq.com>
2019-10-22 06:03:37 +0800

    feat: 使用装饰器优化react-redux中的connect写法

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { connect } from 'react-redux';
 
 import { addCounter, deleteCounter, addCounterAsync } from './index.redux';
 
-export default function App(props) {
-  const { num, addCounter, deleteCounter, addCounterAsync } = props;
-  return(
-    <div>
-      <h1>Counter</h1>
-      <p>现在的计数器: {num}</p>
-      <button onClick={addCounter}>新增计数</button>
-      <button onClick={addCounterAsync}>新增计数(延迟1秒)</button>
-      <button onClick={deleteCounter}>减少计数</button>
-    </div>
-  )
+@connect(
+  state => ({num: state}), 
+  { addCounter, deleteCounter, addCounterAsync }
+)
+class App extends React.Component {
+  render() {
+    const { num, addCounter, deleteCounter, addCounterAsync } = this.props;
+    return(
+      <div>
+        <h1>Counter</h1>
+        <p>现在的计数器: {num}</p>
+        <button onClick={addCounter}>新增计数</button>
+        <button onClick={addCounterAsync}>新增计数(延迟1秒)</button>
+        <button onClick={deleteCounter}>减少计数</button>
+      </div>
+    )
+  }
 }
 
-const mapStatetoProps = (state) => {
-  return { num: state };
-}
-
-const actionCreators = { addCounter, deleteCounter, addCounterAsync }
-
-App = connect(mapStatetoProps, actionCreators)(App);
+export default App;
