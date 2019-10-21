23f4520e3554e2604a3720749f9293dbca6635b4
JanYLee <694730359@qq.com>
2019-10-21 15:22:59 +0800

    feat: 新增ui库ant-design-mobile, 并写入简单示例

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button, List } from 'antd-mobile';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parents></Parents>
     </div>
   );
 }
+
+function Parents() {
+  const boss = 'Parents'
+  return (
+    <div>
+      <h2>父组件: {boss}</h2>
+      <Child1 name='1号'></Child1>
+      <Child2 name='2号'></Child2>
+    </div>
+  )
+}
+
+function Child1(props) {
+  return <h3>子组件1: {props.name}</h3>
+}
+
+class Child2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []      
+    }
+  }
+
+  handleClick = () => {
+    const { list } = this.state;
+    const newItem = {name: 'jay', age: 18}
+    this.setState({list: [...list, newItem]});
+  }
+
+  render() {
+    const { name } = this.props;
+    const { list } = this.state;
+    
+    return(
+      <div>
+        <h3>子组件2: { name }</h3>
+        <Button onClick={this.handleClick}>点击新增计数</Button>
+        <p>计数器: { list.length }</p>
+        <List renderHeader={() => '计数列表'}>
+          {list.map((ele, i) => <List.Item key={i}>{ele.name}, {ele.age}</List.Item>)}
+        </List>
+      </div>
+    )
+  }
+}
+
 
 export default App;
