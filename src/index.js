24300ab25068d133d77fbfd5f92d270f443f5c01
JanYLee <694730359@qq.com>
2019-10-22 23:35:43 +0800

    feat: 新增基础插件和配置
    react-thunk, react-redux和开发插件配置, react-router-dom, 装饰器插件和配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { counter } from './index.redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from './App.jsx';
+
+const reduxDevTools = window.devToolsExtension
+  ? window.devToolsExtension()
+  : f => f;
+// 新建store
+const store = createStore(
+  counter,
+  compose(
+    applyMiddleware(thunk),
+    reduxDevTools
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={App}></Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/index.redux.js
@@ -0,0 +1,33 @@
+const ADD_COUNTER = 'add';
+const DELETE_COUNTER = 'delete';
+
+// 新建store
+// 通过reducer生成
+// 根据老的state和action 生成新的state
+export function counter(state = 0, action) {
+  switch (action.type) {
+    case ADD_COUNTER:
+      return state + 1;
+    case DELETE_COUNTER:
+      return state - 1;
+    default:
+      return 10;
+  }
+}
+
+// actionCreater
+export function addCounter() {
+  return { type: ADD_COUNTER };
+}
+
+export function addCounterAsync() {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(addCounter());
+    }, 1000);
+  };
+}
+
+export function deleteCounter() {
+  return { type: DELETE_COUNTER };
+}
