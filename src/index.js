4110e7e4be4365f559fe7f7989b38d620d4863ef
JanYLee <694730359@qq.com>
2019-10-22 19:35:27 +0800

    feat: react-router4基础使用示例
    BrowserRouter, Route, Link, Switch, Redirect 组件使用, useParams获取路径尾部api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link, Switch, Redirect, useParams } from 'react-router-dom';
 
 import { counter } from './index.redux';
 
@@ -20,9 +21,49 @@ const store = createStore(
   )
 );
 
+function Secend () {
+  return <h2>第二个组件</h2>
+}
+
+function Third () {
+  return <h2>第三个组件</h2>
+}
+
+function Test (props) {
+  console.log(props);
+  const { location } = useParams();
+  return <h2>测试组件, 手动获取:{props.match.params.location}, useParams的api获取: {location}</h2>;
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>root</Link>
+          </li>
+          <li>
+            <Link to='/second'>二组件</Link>
+          </li>
+          <li>
+            <Link to='/third'>三组件</Link>
+          </li>
+          <li>
+            <Link to='/old-match'>重定向到二组件</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path='/' exact component={App}></Route>
+          <Route path='/second' component={Secend}></Route>
+          <Route path='/third' component={Third}></Route>
+          <Route path="/old-match">
+            <Redirect to="/second" />
+          </Route>
+          <Route path='/:location' component={Test}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
