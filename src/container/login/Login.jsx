5dcba790a09bfb09abf084b44689b48b6599ba2d
JanYLee <694730359@qq.com>
2019-10-24 03:17:40 +0800

    feat: 新增登录页面逻辑

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
 
-import Logo from '../../component/logo/Logo.jsx';
 
+import Logo from '../../component/logo/Logo.jsx';
+import { login } from '../../redux/user.redux';
+
+@connect(
+  state => state.user,
+  { login }
+)
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: '',
+      pwd: ''
+    };
     this.register = this.register.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
-  
+
   register() {
     this.props.history.push('/register');
   }
 
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    });
+  }
+
+  handleLogin() {
+    this.props.login(this.state);
+  }
+
   render() {
+    const { msg, redirectTo } = this.props;
     return (
       <div>
+        {redirectTo ? <Redirect to={redirectTo}></Redirect> : null}
         <Logo />
         <h2>登录</h2>
         <WingBlank>
           <List>
-            <InputItem>用户</InputItem>
-            <InputItem>密码</InputItem>
+            {msg ? <p className='Error-Msg'>{msg}</p> : null}
+            <InputItem onChange={v => this.handleChange('user', v)}>
+              用户
+            </InputItem>
+            <InputItem
+              type='password'
+              onChange={v => this.handleChange('pwd', v)}
+            >
+              密码
+            </InputItem>
           </List>
           <WhiteSpace />
-          <Button type='primary'>登录</Button>
+          <Button onClick={this.handleLogin} type='primary'>
+            登录
+          </Button>
           <WhiteSpace />
-          <Button onClick={this.register} type='primary'>注册</Button>
+          <Button onClick={this.register} type='primary'>
+            注册
+          </Button>
         </WingBlank>
       </div>
     );
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,7 +1,8 @@
-import Axios from "axios";
+import Axios from 'axios';
 
 import { getRedirectPath } from '../utils';
 
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 const ERROR_MSG = 'ERROR_MSG';
 
@@ -11,13 +12,27 @@ const initState = {
   user: '',
   pwd: '',
   type: '',
-  redirectTo: '', // 注册成功后跳转的地址
-}
+  redirectTo: '' // 注册成功后跳转的地址
+};
 
-export function user (state=initState, action) {
-  switch(action.type) {
+export function user(state = initState, action) {
+  switch (action.type) {
     case REGISTER_SUCCESS:
-      return { ...state, msg: '', isAuth: true, redirectTo: getRedirectPath(action.payload), ...action.payload }
+      return {
+        ...state,
+        msg: '',
+        isAuth: true,
+        redirectTo: getRedirectPath(action.payload),
+        ...action.payload
+      };
+    case LOGIN_SUCCESS:
+        return {
+          ...state,
+          msg: '',
+          isAuth: true,
+          redirectTo: getRedirectPath(action.payload),
+          ...action.payload
+        };
     case ERROR_MSG:
       return { ...state, isAuth: false, msg: action.msg };
     default:
@@ -25,26 +40,42 @@ export function user (state=initState, action) {
   }
 }
 
-function registerSuccess (data) {
+function loginSuccess (data) {
+  return {type: LOGIN_SUCCESS, payload: data}
+}
+
+function registerSuccess(data) {
   return { type: REGISTER_SUCCESS, payload: data };
 }
 
-function errorMsg (msg) {
+function errorMsg(msg) {
   return { msg, type: ERROR_MSG };
 }
 
-export function register ({user, pwd, repeatPwd, type}) {
-  if(!user || !pwd || !type) return errorMsg('用户名密码必须输入');
-  if(pwd !== repeatPwd) return errorMsg('密码和确认密码不相同');
+export function login ({user, pwd}) {
+  if (!user || !pwd) return errorMsg('用户名密码必须输入');
   return dispatch => {
-    Axios.post('/user/register', { user, pwd, type })
-      .then(res => {
-        if(res.status === 200 && res.data.code === 0) {
-          dispatch(registerSuccess({ user, pwd, type }))
-        } else {
-          dispatch(errorMsg(res.data.msg));
-        }
-      })
-  }
-  
+    Axios.post('/user/login', { user, pwd }).then(res => {
+      if (res.status === 200 && res.data.code === 0) {
+        dispatch(loginSuccess({ user, pwd }));
+      } else {
+        dispatch(errorMsg(res.data.msg));
+      }
+    });
+  };
+}
+
+
+export function register({ user, pwd, repeatPwd, type }) {
+  if (!user || !pwd || !type) return errorMsg('用户名密码必须输入');
+  if (pwd !== repeatPwd) return errorMsg('密码和确认密码不相同');
+  return dispatch => {
+    Axios.post('/user/register', { user, pwd, type }).then(res => {
+      if (res.status === 200 && res.data.code === 0) {
+        dispatch(registerSuccess({ user, pwd, type }));
+      } else {
+        dispatch(errorMsg(res.data.msg));
+      }
+    });
+  };
 }
