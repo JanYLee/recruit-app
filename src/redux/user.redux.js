c0ebff4c06f2ea60ef2a0e8e27dfd7604b4b4cb3
JanYLee <694730359@qq.com>
2019-10-29 01:50:01 +0800

    fix: 修复已填写用户信息后, 重新登录又必须填写用户信息一次的跳转问题

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -61,8 +61,7 @@ export function login({ user, pwd }) {
   return dispatch => {
     Axios.post('/user/login', { user, pwd }).then(res => {
       if (res.status === 200 && res.data.code === 0) {
-        const { type } = res.data.data;
-        dispatch(authSuccess({ user, pwd, type }));
+        dispatch(authSuccess({ ...res.data.data }));
       } else {
         dispatch(errorMsg(res.data.msg));
       }
@@ -76,7 +75,7 @@ export function register({ user, pwd, repeatPwd, type }) {
   return dispatch => {
     Axios.post('/user/register', { user, pwd, type }).then(res => {
       if (res.status === 200 && res.data.code === 0) {
-        dispatch(authSuccess({ user, pwd, type }));
+        dispatch(authSuccess({ ...res.data.data }));
       } else {
         dispatch(errorMsg(res.data.msg));
       }
