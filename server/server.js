76f34b28879bbf770c4a440fc6d118c5b114699c
JanYLee <694730359@qq.com>
2019-10-31 17:22:06 +0800

    feat: 创建聊天页输入框, 并使聊天消息广播到全局

--- server/server.js
+++ server/server.js
@@ -2,13 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('sendmsg', function (data) {
+    console.log(data);
+    io.emit('recvmsg', data)
+  })
+})
+
 const userRouter = require('./user');
 
-const app = express();
 app.use(cookieParser()); // 解析cookie
 app.use(bodyParser.json()); // 解析post传递过来的参数
 app.use('/user', userRouter);
 
-app.listen(9093, function() {
+server.listen(9093, function() {
   console.log("node server start at port 9093");
 });
--- src/component/chat/Chat.jsx
+++ src/component/chat/Chat.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import { List, InputItem } from 'antd-mobile';
+import io from 'socket.io-client';
+
+const socket = io('ws://localhost:9093');
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '', msg: [] };
+  }
+
+  componentDidMount() {
+    socket.on('recvmsg', data => {
+      this.setState({
+        msg: [...this.state.msg, data.text]
+      });
+    });
+  }
+
+  handleSubmit() {
+    socket.emit('sendmsg', { text: this.state.text });
+    this.setState({ text: '' });
+  }
+
   render() {
+    const { text, msg } = this.state;
     return (
       <div>
-        chat with user: {this.props.match.params.user}
+        {msg.map(v => (
+          <p key={v}>{v}</p>
+        ))}
+        <div className='stick-footer'>
+          <List>
+            <InputItem
+              placeholder='请输入'
+              value={text}
+              onChange={v => this.setState({ text: v })}
+              extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+            >
+              信息
+            </InputItem>
+          </List>
+        </div>
       </div>
     );
   }
